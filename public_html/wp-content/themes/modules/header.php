520a20713486f713745d3510b347e5273b08d239
unsognoverde <unsognoverde@gmail.com>
2016-02-02 22:20:52 +0100

    add head icons

--- public_html/wp-content/themes/modules/header.php
+++ public_html/wp-content/themes/modules/header.php
@@ -10,6 +10,24 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_bloginfo('template_directory');?>/images/icons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_bloginfo('template_directory');?>/images/icons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_bloginfo('template_directory');?>/images/icons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_bloginfo('template_directory');?>/images/icons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_bloginfo('template_directory');?>/images/icons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_bloginfo('template_directory');?>/images/icons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_bloginfo('template_directory');?>/images/icons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_bloginfo('template_directory');?>/images/icons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_bloginfo('template_directory');?>/images/icons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_bloginfo('template_directory');?>/images/icons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_bloginfo('template_directory');?>/images/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_bloginfo('template_directory');?>/images/icons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_bloginfo('template_directory');?>/images/icons/favicon-16x16.png">
+<link rel="manifest" href="<?php echo get_bloginfo('template_directory');?>/images/icons/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?php echo get_bloginfo('template_directory');?>/images/icons/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
 <?php // Retrieve Theme Options
 global $quadro_options;
 $quadro_options = quadro_get_options(); ?>
